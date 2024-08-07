5d4351a45d54aa4c130acfa99ba3a0ae5093d293
Abdullah <abdullahibniwan@gmail.com>
2024-08-07 13:48:17 +0700

    v3 and finish

---
+++ localhost/script.js
@@ -0,0 +1,29 @@
+function calculate() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const operation = document.getElementById('operation').value;
+    let result;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        result = "Masukkan angka yang valid";
+    } else {
+        switch (operation) {
+            case 'add':
+                result = num1 + num2;
+                break;
+            case 'subtract':
+                result = num1 - num2;
+                break;
+            case 'multiply':
+                result = num1 * num2;
+                break;
+            case 'divide':
+                result = num1 / num2;
+                break;
+            default:
+                result = "Operasi tidak valid";
+        }
+    }
+
+    document.getElementById('result').innerText = `Hasil: ${result}`;
+}
